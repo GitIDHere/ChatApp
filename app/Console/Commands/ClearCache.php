e89bc5ef18cd810a6afb56e9ab11ddedfb111c8a
Samir Vora <sosolid1236@gmail.com>
2020-05-31 14:53:25 +0100

    initial commit

---
+++ app/Console/Commands/ClearCache.php
@@ -0,0 +1,50 @@
+<?php namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+
+class ClearCache extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'ClearCache';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Clear the artisan and composer cache';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+		Artisan::call('config:clear');
+		Artisan::call('cache:clear');
+		Artisan::call('cache:clear');
+
+		exec('composer dump-autoload');
+
+		Artisan::call('view:clear');
+		Artisan::call('route:clear');
+
+		$this->info('Cache cleared');
+    }
+}
